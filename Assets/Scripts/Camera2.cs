4f3a9cad105e8440a5a1cad5d03899f5ef58842f
mezleca <mtksenpai@gmail.com>
2026-02-17 16:42:57 -0300

    rest

---
+++ Assets/Scripts/Camera2.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Camera2 : MonoBehaviour
+{
+    [SerializeField] public Transform player_transform;
+    [SerializeField] public float lerp_t = 100.0f; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() {
+        if (player_transform == null) {
+            Debug.Log("how");
+            return;
+        }
+
+        Vector3 target_pos = player_transform.position;
+        target_pos.z -= 5;
+
+        transform.position = Vector3.Lerp(transform.position, target_pos, lerp_t * Time.deltaTime);
+    }
+}
